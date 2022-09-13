a51401b33d76215c065f3cf2f9493d3f7fdd53e1
Boyan Kloosterman <93587280+BoyanKloosterman@users.noreply.github.com>
2022-09-13 10:48:53 +0200

    Delete C1 directory

--- C1/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <div class="footerContact">
-        <h3>Contact</h3>
-        <p>Telefoon: 088-209-8000</p>
-        <p>Email: infopunt@curio.nl</p>
-    </div>
-    <p>Opendag &copy</p>
-</footer>
