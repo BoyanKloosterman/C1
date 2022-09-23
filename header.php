9fdd27a55cfef7deb3bf0209085739a3b042121b
Boyan Kloosterman <boyankloosterman1@gmail.com>
2022-09-23 10:56:19 +0200

    pagina's verwijderd

--- header.php
+++ header.php
@@ -2,14 +2,11 @@
     <div class="toplogo">
         <img src="img/curio-15-licht-groen-logo-rgb.png">
     </div>
-    <nav>
-        <a href="vakken.php">Vakken</a>
-        <a href="rooster.php">Rooster</a>
-        <a href="indeling.php">Indeling</a>
-        <a href="versnellen.php">Versnellen</a>
-        <a href="vakanties.php">Vakanties</a>
-        <a href="sfeer.php">Sfeer</a>
-    </nav>
+    <div class="curiofotos">
+    <marquee behavior="scroll" direction="left">
+        <img src="img/" alt="logo">
+    </marquee>
+</div>
     <div class="name">
         <a>Boyan Kloosterman - SSD2E</a>
     </div>
