40dc55e9bf56722ebe2129b5af8c30429628ae2b
Boyan Kloosterman <boyankloosterman1@gmail.com>
2022-09-23 11:04:54 +0200

    picture scroll onder div gedaan

--- header.php
+++ header.php
@@ -1,13 +1,16 @@
  <div class="header">
-    <div class="toplogo">
-        <img src="img/curio-15-licht-groen-logo-rgb.png">
-    </div>
-    <div class="curiofotos">
-    <marquee behavior="scroll" direction="left">
-        <img src="img/" alt="logo">
-    </marquee>
-</div>
-    <div class="name">
-        <a>Boyan Kloosterman - SSD2E</a>
-    </div>
-</div>
+     <div class="toplogo">
+         <img src="img/curio-15-licht-groen-logo-rgb.png">
+     </div>
+     <div class="h1title">
+         <h1>Open Dag</h1>
+        </div>
+     <div class="name">
+         <a>Boyan Kloosterman - SSD2E</a>
+     </div>
+ </div>
+ <div class="curiofotos">
+         <marquee behavior="scroll" direction="left">
+             <img src="img/" alt="logo">
+         </marquee>
+     </div>
